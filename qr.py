f529b386628acba305c2007121b5cfe567bf21dd
biraj094 <koiralabiraj@gmail.com>
2020-02-02 16:41:34 +0545

    download feature working. Pending: Display qrcode

--- qr.py
+++ qr.py
@@ -1,30 +1,26 @@
 from flask import request
 import qrcode
 from flask import send_file
-from PIL import Image
+# from PIL import Image
+
 
 def getdata():
     name=request.form['n']
     return name
 
-def qrfunc(data):
+def qrfunc(link):
     qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_L,box_size=15, border=10  )
-    qr.add_data(data)
+    qr.add_data(link)
     qr.make(fit=True)
     img=qr.make_image(fill="black",back_color="white")
 
-    #TODO: want to generate the image and be able to download it withour saving it anywhere
-    #TODO: download can be done by pressing the button 
-    #TODO: Be able to name the file 
-    #TODO:  
-    jpgfile = Image.open(img)
-    filename='image.png'
+    return img
 
-    # img.save('/Users/apple/Desktop/Biraj/Contact card/static/images/YourContactCard.png')
-    return send_file(jpgfile,
-                 attachment_filename=filename,
-                 mimetype='image/png',
-                 as_attachment=True)
+
+
+
+
+    
 
     
 
--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 from flask import Flask,render_template
 from qr import getdata,qrfunc
+import io
+from tempfile import NamedTemporaryFile
+# import Image
+from PIL import Image
+from flask import send_file
 
 app=Flask(__name__)
 
@@ -9,10 +14,45 @@ def index():
 
 @app.route('/display',methods=['POST','GET'])
 def display():
-    n=getdata()
-    # qrimage=qrfunc(n)
+    link=getdata()
+    qrimage=qrfunc(link)
+    # final= qrimage.show()
     # return render_template('display.html',qrimage=qrimage)
-    return qrfunc(n)
+    return render_template('display.html', link=link )
+
+@app.route('/download/<link>',methods=['POST','GET'])
+def download(link):
+    linkk=link
+    qrimage=qrfunc(linkk)
+    filename='image.png'
+    with NamedTemporaryFile(delete=True) as tmp:
+        qrimage.save(tmp.name)
+        str_io = io.BytesIO(tmp.read())
+
+    # img.save('/Users/apple/Desktop/Biraj/Contact card/static/images/YourContactCard.png')
+    return send_file(str_io,
+                 attachment_filename=filename,
+                 mimetype='image/png',
+                 as_attachment=True)
+
+    # return render_template('display.html', qrimage=qrimage)
+
+
+# @app.route('/download<qrimage>', methods=['GET'])
+# def download(qrimage):
+#     filename='image.png'
+
+
+#     im = Image.open(qrimage,'r')
+#     with NamedTemporaryFile(delete=True) as tmp:
+#         im.save(tmp.name)
+#         str_io = io.BytesIO(tmp.read())
+
+#     # img.save('/Users/apple/Desktop/Biraj/Contact card/static/images/YourContactCard.png')
+#     return send_file(str_io,
+#                  attachment_filename=filename,
+#                  mimetype='image/png',
+#                  as_attachment=True)
 
 
 if __name__=='__main__':
