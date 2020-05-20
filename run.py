7199fa12462d00f6769def2786fd4066449bb729
biraj094 <koiralabiraj@gmail.com>
2020-05-20 22:22:43 +0545

    Filename feature added

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template , request,send_file
-from qr import getdata,qrfunc
+from qr import qrfunc
 import io
 from tempfile import NamedTemporaryFile
 
@@ -12,17 +12,22 @@ def index():
 
 @app.route('/display',methods=['POST','GET'])
 def display():
-    link = request.form['n']
+    link = request.form['link']
     return render_template('display.html', link=link )
 
 @app.route('/download/<path:link>',methods=['POST','GET'])
 def download(link):
     link=link
-    qrimage=qrfunc(link)
-    filename='image.png'
+    qrimage=qrfunc(link) 
     with NamedTemporaryFile(delete=True) as tmp:
         qrimage.save(tmp.name)
         str_io = io.BytesIO(tmp.read())
+    filename="image.png"
+    if request.method == "POST":
+        filename = "image.png" 
+        filename_byuser = request.form['filename'] + ".png"
+        if len(filename_byuser)>4:
+            filename = filename_byuser
     return send_file(str_io,
                  attachment_filename=filename,
                  mimetype='image/png',
